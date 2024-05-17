d1afef2a0cb1e5bcea9ca063bc91446cc1f24e5d
Puripat Kitkarncharoensin <puripat.k@arise.tech>
2024-05-17 09:28:11 +0700

    Test Fizz Buzz Input = 8 #1

--- testgo/fizzbuzz_test.go
+++ testgo/fizzbuzz_test.go
@@ -78,3 +78,14 @@ func TestFizzBuzzShouldReturn7WhenInput7(t *testing.T) {
 		t.Errorf("got %q but want %q", got, want)
 	}
 }
+
+func TestFizzBuzzShouldReturn8WhenInput8(t *testing.T) {
+	input := 8
+
+	got := FizzBuzz(input)
+
+	want := "8"
+	if got != want {
+		t.Errorf("got %q but want %q", got, want)
+	}
+}
