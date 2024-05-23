8da0a4c55e47d454fac125842506ba4da82fa438
Puripat Kitkarncharoensin <puripat.k@arise.tech>
2024-05-23 15:45:02 +0700

    Origin

--- testgo/fizzbuzz.go
+++ testgo/fizzbuzz.go
@@ -1,42 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-	"strings"
-)
+import "fmt"
 
-func FizzBuzz(n int) string {
-	if n%15 == 0 {
-		return "FizzBuzz"
-	} else if n%5 == 0 {
-		return "Buzz"
-	} else if n%3 == 0 {
-		return "Fizz"
-	} else {
-		return strconv.Itoa(n)
-	}
-}
-
-func FizzBuzzLevl2(n int) string {
-	Fizz := n%3 == 0
-	Buzz := n%5 == 0
-	FizzBuzz := Fizz && Buzz
-	number := Fizz || Buzz || FizzBuzz
-
-	result := fmt.Sprintf("%s%s%s",
-		ternary(Fizz, "Fizz", ""),
-		ternary(Buzz, "Buzz", ""),
-		ternary(number, "", strconv.Itoa(n)),
-	)
-	result = strings.ReplaceAll(result, " ", "")
-
-	return result
-}
-
-func ternary(condition bool, trueVal, falseVal string) string {
-	if condition {
-		return trueVal
-	}
-	return falseVal
+func FizzBuzzLevl3(n int) string {
+	result := map[bool]string{true: "Fizz", false: ""}[n%3 == 0]
+	result += map[bool]string{true: "Buzz", false: ""}[n%5 == 0]
+	return map[bool]string{true: result, false: fmt.Sprint(n)}[result != ""]
 }
